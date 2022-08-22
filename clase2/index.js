2f6a82eb68d0848437f039dcb204e5c678f56fef
Anabel Rossi Rios <anabelrossi40@gmail.com>
2022-08-22 20:31:52 -0300

    first commit

---
+++ clase2/index.js
@@ -0,0 +1,44 @@
+
+
+/*let nombre = "yani";
+if (nombre ==="yani") {
+    console.log("Hola Yani");
+    }
+console.log("chau yani");
+
+let numero = 10;
+
+if(numero === 30){
+console.log("eres igual a 30");
+} else{
+    console.log("no eres igual a 30");
+}
+console.log("terminamos");
+
+let respuesta = prompt ("terminaste la tarea?");
+
+if(respuesta === "si"){
+    alert("puedes salir a jugar");
+} else{
+    alert("no puedes salir a jugar");
+}
+
+/* combinados */
+
+/*let respuestaTarea = prompt ("terminaste la tarea?");
+let respuestaCasa = prompt ("ordenaste la casa?");
+
+if(respuestaCasa === "si" && respuestaTarea === "si"){
+    alert("puedes sallir a jugar");
+} else{
+    alert("no pudes salir a jugar");
+}*/
+
+let nombre = prompt("ingrese el nombre");
+
+if(( nombre == "andres" || nombre == "ANDRES") && (nombre !== "")) {
+    alert("Hola andres")
+} else {
+alert("Datos incorrectos");}
+
+
---
+++ clase3/index.js
@@ -0,0 +1,8 @@
+/* TURNOS */
+
+for(let i = 1; i <= 15; i++){
+    let nombre = prompt("Ingrese su nombre");
+    let mensaje = `Turno Nº ${i} Nombre: ${nombre}`;
+    alert(mensaje);
+  }
+  alert("SIN TURNOS DISPONIBLES");
