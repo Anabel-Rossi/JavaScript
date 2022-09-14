80c67b41adbfb234d3f7ad0368c1b2aba785faf1
Anabel Rossi Rios <anabelrossi40@gmail.com>
2022-09-14 19:20:21 -0300

    Eventos prueba

---
+++ Eventos/stock.js
@@ -0,0 +1,43 @@
+const productos = [
+    {
+        id:1,
+        nombre:"3060",
+        desc:"Gafas qesi",
+        precio:90000,
+        img:'src/gafas1.jpg',
+        cantidad:1
+    },
+    {
+        id:2,
+        nombre:"Bi-Focal",
+        desc:"Grafica",
+        precio:1500,
+        img:'src/gafas2.jpg',
+        cantidad:1
+    },
+    {
+        id:3,
+        nombre:"Fem",
+        desc:"Grafica",
+        precio:2000,
+        img:'src/gafas3.jpg',
+        cantidad:1
+    },
+    {
+        id:4,
+        nombre:"Ray ban",
+        desc:"Grafica",
+        precio:2000,
+        img:'src/gafas6.jpg',
+        cantidad:1
+    },
+    {
+        id:5,
+        nombre:"Elegant Sport",
+        desc:"Grafica",
+        precio:3000,
+        img:'src/gafas7.jpg',
+        cantidad:1
+    },
+]
+
